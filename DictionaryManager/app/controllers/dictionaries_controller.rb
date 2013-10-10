4b8b0d216c827d515f5de97a911b8d11c5852f50
Han-Cheol Cho <priancho@gmail.com>
2013-10-10 16:04:13 +0900

    DictionaryManager: simstring path is fixed to PubDictionaries/simstring-1.0

--- DictionaryManager/app/controllers/dictionaries_controller.rb
+++ DictionaryManager/app/controllers/dictionaries_controller.rb
@@ -1,4 +1,5 @@
-require File.join( File.dirname(__FILE__), '/simstring-1.0/swig/ruby/simstring')
+# require File.join( File.dirname(__FILE__), '/simstring-1.0/swig/ruby/simstring')
+require File.join( Rails.root, '..', 'simstring-1.0/swig/ruby/simstring')
 
 
 class DictionariesController < ApplicationController
