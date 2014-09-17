33f1ce68ff1ffb9c6faa72d8db271cf2e7a0ae90
Mac mini <tahara@digital-squad.net>
2014-09-17 15:51:31 +0900

    Revised unfinished icon style

--- PubDictionaries/app/helpers/dictionaries_helper.rb
+++ PubDictionaries/app/helpers/dictionaries_helper.rb
@@ -39,9 +39,11 @@ module DictionariesHelper
   def dictionary_status(dictionary)
     if dictionary.unfinished?
       css_class = 'unfinished_icon'
+      title = 'Unfinished'
     else
       css_class = 'finished_icon'
+      title = 'Finished'
     end
-    content_tag :span, nil, class: css_class
+    content_tag :span, nil, class: css_class, title: title
   end
 end
