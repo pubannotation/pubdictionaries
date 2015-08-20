43d293f3e3dc0ef4736a437884dd84627bb72e23
Mac mini <tahara@digital-squad.net>
2015-08-20 11:02:27 +0900

    changed file directory

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,16 +26,3 @@ $(document).ready(function(){
   });
 });
 
-var toggleDictionary = function(e){
-  var isSearched = e.parent().attr('id') == 'search_target_dictionaries';
-  $(e).remove();
-  if (isSearched){
-    $(e).children('i').removeClass('fa-times-circle');
-    $(e).children('i').addClass('fa-plus-circle');
-    $('#dialog').append(e);
-  }else{
-    $(e).children('i').addClass('fa-times-circle');
-    $(e).children('i').removeClass('fa-plus-circle');
-    $('#search_target_dictionaries').append(e);
-  }
-};
