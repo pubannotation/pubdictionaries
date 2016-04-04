992d4d1912e1b0a79bb5780bdec078a69def55b7
Jin-Dong Kim <jindong.kim@gmail.com>
2016-04-05 00:56:09 +0900

    CLEANED unnecessary codes

--- app/helpers/dictionaries_helper.rb
+++ app/helpers/dictionaries_helper.rb
@@ -7,18 +7,6 @@ module DictionariesHelper
   	end
   end
 
-  # Check if an entry is marked as disabled or not.
-  def disabled?(user_dictionary, entry)
-  	# Rails.logger.debug entry.inspect
-  	if user_dictionary
-	  disabled_entries = user_dictionary.removed_entries
-      if disabled_entries and disabled_entries.where(entry_id: entry.id).first 
-        return true
-      end
-    end
-    return false
-  end
-
   def format_dic_names(diclist_json)
     if diclist_json.nil?
       return ""
