6e5ffcac9ddc7bf26247f084791da02e7bf850b4
Jin-Dong Kim <jdkim@dbcls.rois.ac.jp>
2018-02-15 11:20:43 +0900

    ADDED application of dos2unix to a uploaded file (a temporary solution)

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -15,6 +15,9 @@ class EntriesController < ApplicationController
         target_filepath = File.join('tmp', "upload-#{dictionary.name}-#{Time.now.to_s[0..18].gsub(/[ :]/, '-')}")
         FileUtils.cp source_filepath, target_filepath
 
+        # TODO: at the moment, it is hard-coded. It should be improved.
+        `/usr/bin/dos2unix #{target_filepath}`
+
         # job = LoadEntriesFromFileJob.new(target_filepath, dictionary)
         # job.perform
 
