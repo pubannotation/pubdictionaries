cf0a972d8f81e3af076a8e4711f2cef90c08a6fb
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2019-01-10 17:56:54 +0900

    refactor: Delete source code not used from user's model and controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,4 @@
 class UsersController < ApplicationController
-  before_filter :is_root_user?, only: :index
-
-  def index
-    @users = User.all.page(params[:page]) 
-  end
-
   def show
     @user = User.find_by_username(params[:name])
     @dictionaries_grid = initialize_grid(Dictionary.mine(@user),
--- app/models/user.rb
+++ app/models/user.rb
@@ -15,28 +15,4 @@ class User < ActiveRecord::Base
 
   validates :username, :presence => true, :length => {:minimum => 5, :maximum => 20}, uniqueness: true
   validates_format_of :username, :with => /\A[a-z0-9][a-z0-9_-]+\z/i
-
-  # Override the original to_param so that it returns name, not ID, for constructing URLs.
-  # Use Model#find_by_name() instead of Model.find() in controllers.
-  def to_param
-    name
-  end
-
-  # Get the user ID for the given email/password pair.
-  def self.get_user_id(params)
-    if params.nil? or params["email"] == nil or params["email"] == ""
-      return nil
-    else
-      # Find the user that first matches to the condition.
-      user = User.find_by_email(params["email"])
-
-      if user and user.valid_password?(params["password"])
-        return user.id
-      else
-        return :invalid
-      end
-    end
-  end
-
-  
 end
