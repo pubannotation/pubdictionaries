771f1604e9b702a13216e6adea4f42d7548b29a5
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2019-01-10 18:47:42 +0900

    refactor: Delete source code not used from application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  after_filter :store_location
-
-  def store_location
-    # It causes redirection loop after /user/password/edit
-    #
-    # # store last url - this is needed for post-login redirect to whatever the user last visited.
-    # if (request.fullpath != "/users/sign_in" &&
-    #   request.fullpath != "/users/sign_up" &&
-    #   request.fullpath != "/users/password" &&
-    #   !request.xhr?) # don't store ajax calls
-
-    #   if request.format == "text/html" || request.content_type == "text/html"
-    #     session[:previous_url] = request.fullpath
-    #     session[:last_request_time] = Time.now.utc.to_i
-    #   end
-    # end
-  end
 
   def after_sign_in_path_for(resource)
     valid_time_span = 120     # 120 seconds
