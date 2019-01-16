9e107fe3861b38beab4990a2b5568538a87ebb18
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2019-01-16 13:51:59 +0900

    rails4.0: Set strong parameters to signup by the Devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation) }
+  end
 
   def after_sign_in_path_for(resource)
     valid_time_span = 120     # 120 seconds
