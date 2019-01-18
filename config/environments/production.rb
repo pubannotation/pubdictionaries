8109d4501e8bd9d0467b12f9232903b870bea53f
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2019-01-18 17:41:20 +0900

    rails4.0: Name of the config.assets.compress option was changed to js_compressor

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,8 +27,8 @@ PubDictionaries::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = true
 
-  # Compress JavaScripts and CSS
-  config.assets.compress = true
+  # Compress JavaScripts
+  config.assets.js_compressor = :uglifier
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
