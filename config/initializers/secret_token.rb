ea88f8ab529d36eaac8d41e49c24e601a7c45fd7
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2019-01-30 14:45:12 +0900

    rails4.1: Use config/secret.yml

--- config/initializers/secret_token.rb
+++
@@ -1,3 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-PubDictionaries::Application.config.secret_key_base = 'b3c052db926d93538f3634c9464963bb89b2032395c7b622d7449345bcc6667966afc9f6d242732fe8dda453af4880ee8d1025a1b38003c8289afe46b2f8dd27'
