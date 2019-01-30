bd9d237902289b595175c6a456387c351aeeda45
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2019-01-30 15:37:35 +0900

    rails4.1: Implicit join references were removed with Rails 4.1

--- config/application.rb
+++ config/application.rb
@@ -47,7 +47,6 @@ module PubDictionaries
     # This is necessary if your schema can't be completely dumped by the schema dumper,
     # like if you have constraints or database-specific column types
     # config.active_record.schema_format = :sql
-    config.active_record.disable_implicit_join_references = true
 
     # Enable the asset pipeline
     config.assets.enabled = true
