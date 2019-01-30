7cc8a3e60959f20597de6242e96f837f4e0f0656
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2019-01-30 17:43:32 +0900

    rails5.0: Passing strings or symbols to the middleware builder is deprecated

--- config/application.rb
+++ config/application.rb
@@ -57,7 +57,7 @@ module PubDictionaries
   	# Use SSL
   	# config.force_ssl = true
 
-    config.middleware.insert_before 0, "Rack::Cors" do
+    config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
         resource '*', :headers => :any, :methods => [:get, :post, :options]
