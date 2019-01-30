5f21de58b52d183425148e47f45a73de53d6182d
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2019-01-30 14:41:06 +0900

    rails4.1: Devise 4.5 changes the name of Devise::ParameterSanitizer#for method to permit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation) }
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation) }
   end
 
   def after_sign_in_path_for(resource)
