3e0a1226a41b6b24a031da392e0015ac556132a7
shar sukhchuluun <sukh@luxiarsukh.local>
2020-09-04 10:40:51 +0900

    issue #4 google認証機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :confirmable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, :omniauth_providers => [:google_oauth2]
 
   has_many :dictionaries, dependent: :destroy
   has_many :associations
@@ -11,4 +12,16 @@ class User < ApplicationRecord
 
   validates :username, :presence => true, :length => {:minimum => 5, :maximum => 20}, uniqueness: true
   validates_format_of :username, :with => /\A[a-z0-9][a-z0-9_-]+\z/i
+
+  def self.from_omniauth(auth)
+    user = User.find_by_email(auth.info.email)
+    return user if user and user.confirmed?
+
+    user = User.new(email: auth.info.email,
+                    username: auth.info.name,
+                    password: Devise.friendly_token[0,20]
+                   )
+    user.save
+    user
+  end
 end
