3c44eb8939667544a8440a0c95f328de50f479e9
shar sukhchuluun <sukh@luxiarsukh.local>
2020-09-30 10:59:13 +0900

    issue #4 google認証機能追加：インデント修正

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -16,10 +16,10 @@ module UsersHelper
 	end
 
 	def login_with_google
-<<HEREDOC
-<div style="display:inline-block; color:#fff">
-  <div style="display:inline-block; margin: 0; padding:5px; background-color:#99f"><i class="fa fa-google" style="width:16px; height:15px"></i></div><div style="display:inline-block; margin:0; padding:5px; background-color:#88f"> Log in with Google</div>
-</div>
-HEREDOC
+		<<-HEREDOC
+		<div style="display:inline-block; color:#fff">
+		  <div style="display:inline-block; margin: 0; padding:5px; background-color:#99f"><i class="fa fa-google" style="width:16px; height:15px"></i></div><div style="display:inline-block; margin:0; padding:5px; background-color:#88f"> Log in with Google</div>
+		</div>
+		HEREDOC
   end
 end
