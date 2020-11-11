b25eb644dc6d619cba0c67d70137b7cb87c47b9a
Jin-Dong Kim <Jin-Dong Kim>
2020-11-11 13:48:26 +0900

    STYLE indentation is changed to use tabs

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,84 +1,84 @@
 class EntriesController < ApplicationController
-  # Requires authentication for all actions
-  before_action :authenticate_user!
- 
-  def create
-    begin
-      dictionary = Dictionary.editable(current_user).find_by_name(params[:dictionary_id])
+	# Requires authentication for all actions
+	before_action :authenticate_user!
 
-      raise ArgumentError, "Cannot find the dictionary, #{params[:dictionary_id]}." if dictionary.nil?
+	def create
+		begin
+			dictionary = Dictionary.editable(current_user).find_by_name(params[:dictionary_id])
 
-      dictionary.create_addition(params[:label].strip, params[:identifier].strip)
+			raise ArgumentError, "Cannot find the dictionary, #{params[:dictionary_id]}." if dictionary.nil?
 
-    rescue => e
-      message = e.message
-    end
+			dictionary.create_addition(params[:label].strip, params[:identifier].strip)
 
-    respond_to do |format|
-      format.html { redirect_back fallback_location: root_path, notice: message }
-    end
-  end
+		rescue => e
+			message = e.message
+		end
 
-  def upload_tsv
-    begin
-      dictionary = Dictionary.editable(current_user).find_by_name(params[:dictionary_id])
+		respond_to do |format|
+			format.html { redirect_back fallback_location: root_path, notice: message }
+		end
+	end
 
-      raise ArgumentError, "Cannot find the dictionary, #{params[:dictionary_id]}." if dictionary.nil?
-      raise RuntimeError, "The last task is not yet dismissed. Please dismiss it and try again." if dictionary.jobs.count > 0
+	def upload_tsv
+		begin
+			dictionary = Dictionary.editable(current_user).find_by_name(params[:dictionary_id])
 
-      source_filepath = params[:file].tempfile.path
-      target_filepath = File.join('tmp', "upload-#{dictionary.name}-#{Time.now.to_s[0..18].gsub(/[ :]/, '-')}")
-      FileUtils.cp source_filepath, target_filepath
+			raise ArgumentError, "Cannot find the dictionary, #{params[:dictionary_id]}." if dictionary.nil?
+			raise RuntimeError, "The last task is not yet dismissed. Please dismiss it and try again." if dictionary.jobs.count > 0
 
-      # delayed_job = LoadEntriesFromFileJob.new(target_filepath, dictionary)
-      # delayed_job.perform
+			source_filepath = params[:file].tempfile.path
+			target_filepath = File.join('tmp', "upload-#{dictionary.name}-#{Time.now.to_s[0..18].gsub(/[ :]/, '-')}")
+			FileUtils.cp source_filepath, target_filepath
 
-      delayed_job = Delayed::Job.enqueue LoadEntriesFromFileJob.new(target_filepath, dictionary), queue: :upload
-      Job.create({name:"Upload dictionary entries", dictionary_id:dictionary.id, delayed_job_id:delayed_job.id})
-      message = ''
+			# delayed_job = LoadEntriesFromFileJob.new(target_filepath, dictionary)
+			# delayed_job.perform
 
-    rescue => e
-      message = e.message
-    end
+			delayed_job = Delayed::Job.enqueue LoadEntriesFromFileJob.new(target_filepath, dictionary), queue: :upload
+			Job.create({name:"Upload dictionary entries", dictionary_id:dictionary.id, delayed_job_id:delayed_job.id})
+			message = ''
 
-    respond_to do |format|
-      format.html { redirect_back fallback_location: root_path, notice: message }
-    end
-  end
+		rescue => e
+			message = e.message
+		end
 
-  def destroy
-    begin
-      dictionary = Dictionary.editable(current_user).find_by_name(params[:dictionary_id])
-      raise ArgumentError, "Cannot find the dictionary" if dictionary.nil?
+		respond_to do |format|
+			format.html { redirect_back fallback_location: root_path, notice: message }
+		end
+	end
 
-      entry = Entry.find(params[:id])
-      raise ArgumentError, "Cannot find the entry" if entry.nil?
+	def destroy
+		begin
+			dictionary = Dictionary.editable(current_user).find_by_name(params[:dictionary_id])
+			raise ArgumentError, "Cannot find the dictionary" if dictionary.nil?
 
-      dictionary.create_deletion(entry)
-    rescue => e
-      message = e.message
-    end
+			entry = Entry.find(params[:id])
+			raise ArgumentError, "Cannot find the entry" if entry.nil?
 
-    respond_to do |format|
-      format.html{ redirect_back fallback_location: root_path, notice: message }
-    end
-  end
+			dictionary.create_deletion(entry)
+		rescue => e
+			message = e.message
+		end
 
-  def undo
-    begin
-      dictionary = Dictionary.editable(current_user).find_by_name(params[:dictionary_id])
-      raise ArgumentError, "Cannot find the dictionary" if dictionary.nil?
+		respond_to do |format|
+			format.html{ redirect_back fallback_location: root_path, notice: message }
+		end
+	end
 
-      entry = Entry.find(params[:id])
-      raise ArgumentError, "Cannot find the entry" if entry.nil?
+	def undo
+		begin
+			dictionary = Dictionary.editable(current_user).find_by_name(params[:dictionary_id])
+			raise ArgumentError, "Cannot find the dictionary" if dictionary.nil?
 
-      dictionary.undo_entry(entry)
-    rescue => e
-      message = e.message
-    end
+			entry = Entry.find(params[:id])
+			raise ArgumentError, "Cannot find the entry" if entry.nil?
 
-    respond_to do |format|
-      format.html{ redirect_back fallback_location: root_path, notice: message }
-    end
-  end
+			dictionary.undo_entry(entry)
+		rescue => e
+			message = e.message
+		end
+
+		respond_to do |format|
+			format.html{ redirect_back fallback_location: root_path, notice: message }
+		end
+	end
 end
