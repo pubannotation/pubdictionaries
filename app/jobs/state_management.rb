be2e4b0bc1e4022c5b971d188ebe25bfcb78bbdd
Jin-Dong Kim <Jin-Dong Kim>
2020-11-26 19:54:29 +0900

    FIXED finding the job object of a delayed_job object

--- app/jobs/state_management.rb
+++ app/jobs/state_management.rb
@@ -1,11 +1,17 @@
 module StateManagement
 	def before(delayed_job)
 		@job = Job.find_by_delayed_job_id(delayed_job.id)
+		if @job.nil?
+			sleep(0.1)
+			@job = Job.find_by_delayed_job_id(delayed_job.id)
+		end
+		raise "Could not find its job object" if @job.nil?
+
 		@job.update_attribute(:begun_at, Time.now)
 	end
 
 	def after
-		@job.update_attribute(:ended_at, Time.now) if @job
+		@job.update_attribute(:ended_at, Time.now)
 	end
 
 	def error(job, exception)
