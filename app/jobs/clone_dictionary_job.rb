fca987c5b9bbefe8d2731428d891e861feedb09c
KishiKyosuke <carp011808pg@icloud.com>
2021-02-25 10:18:21 +0900

    REVISED clone dictionary job to ActiveJob

--- app/jobs/clone_dictionary_job.rb
+++ app/jobs/clone_dictionary_job.rb
@@ -1,11 +1,9 @@
-class CloneDictionaryJob < Struct.new(:source_dictionary, :dictionary)
-	include StateManagement
-
-	def perform
+class CloneDictionaryJob < ApplicationJob
+  def perform(source_dictionary, dictionary)
     begin
       dictionary.add_entries(source_dictionary.entries)
     rescue => e
-			@job.message = e.message
+      @job.message = e.message
     end
-	end
+  end
 end
