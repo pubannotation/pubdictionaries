03c9bb9b60c9be25dfcd9dbe6776312db7b7ae73
KishiKyosuke <carp011808pg@icloud.com>
2021-03-18 11:55:45 +0900

    REMOVED delayed_jobs table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_09_061752) do
+ActiveRecord::Schema.define(version: 2021_03_18_024758) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -23,21 +23,6 @@ ActiveRecord::Schema.define(version: 2021_03_09_061752) do
     t.datetime "updated_at", null: false
     t.index ["dictionary_id"], name: "index_associations_on_dictionary_id"
     t.index ["user_id"], name: "index_associations_on_user_id"
-  end
-
-  create_table "delayed_jobs", force: :cascade do |t|
-    t.integer "priority", default: 0, null: false
-    t.integer "attempts", default: 0, null: false
-    t.text "handler", null: false
-    t.text "last_error"
-    t.datetime "run_at"
-    t.datetime "locked_at"
-    t.datetime "failed_at"
-    t.string "locked_by"
-    t.string "queue"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["priority", "run_at"], name: "delayed_jobs_priority"
   end
 
   create_table "dictionaries", force: :cascade do |t|
