4bfcb450dcc10784369980d7b2a79226ea9ff830
Jin-Dong Kim <jindong.kim@gmail.com>
2021-07-05 02:13:45 +0900

    ADDED sidekiq monitoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+require 'sidekiq/web'
+mount Sidekiq::Web => '/sidekiq'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "home/about", as: "about"
 
