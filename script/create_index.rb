b589e4385411523d574c11706b3b81a1a692aa77
ledsun <shigeru.nakajima@gmail.com>
2022-12-14 17:00:08 +0900

    Wait to starting Elasticsearch in the docker environment

--- script/create_index.rb
+++ script/create_index.rb
@@ -1,1 +1,8 @@
-Entry.__elasticsearch__.create_index! force:true
+3.times do
+  puts "Try to create index."
+  Entry.__elasticsearch__.create_index! force:true
+  puts "Index is created."
+rescue Faraday::ConnectionFailed
+  puts "Wait to starting Elasticsearch"
+  sleep 3
+end
