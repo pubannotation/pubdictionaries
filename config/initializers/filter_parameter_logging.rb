170529884e7780b1b9e6fe87032aa2ab3272b227
ledsun <shigeru.nakajima@gmail.com>
2022-12-21 13:38:21 +0900

    Move settings for logging parameters into initializers from the config/application.rb

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -2,3 +2,10 @@
 
 # Configure sensitive parameters which will be filtered from the log file.
 Rails.application.config.filter_parameters += [:password]
+
+# filter out long text parameters
+Rails.application.config.filter_parameters << lambda do |k, v|
+  if k == 'text' && v && v.class == String && v.length > 64
+    v.replace(v[0, 60] + ' ...')
+  end
+end
