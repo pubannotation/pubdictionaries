81e129f7a71c4f1c723fdf56d7334197da732ff1
ledsun <shigeru.nakajima@gmail.com>
2023-02-08 17:07:48 +0900

    Use docker compose healthcheck

--- script/create_index.rb
+++ script/create_index.rb
@@ -1,8 +1,2 @@
-3.times do
-  puts "Try to create index."
-  Entry.__elasticsearch__.create_index! force:true
-  puts "Index is created."
-rescue Faraday::ConnectionFailed
-  puts "Wait to starting Elasticsearch"
-  sleep 3
-end
+Entry.__elasticsearch__.create_index! force:true
+puts "Index is created."
