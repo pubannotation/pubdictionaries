976eb72c9dd8bf12580eded459daf50569a2d011
ledsun <shigeru.nakajima@gmail.com>
2023-03-03 18:36:26 +0900

    unsafe_load elasticsearch.yml with psych4

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -3,7 +3,7 @@ module PubDictionaries
   # Set the setting values to the Rails.configuration so that it can be referred from application code.
   # For exapmle, you can get host of the elasticsearch from the `Rails.configuration.elasticsearch`.
   ELASTICSEARCH_CONFIG_FILE = 'config/elasticsearch.yml'
-  Rails.configuration.elasticsearch = YAML.load_file(ELASTICSEARCH_CONFIG_FILE)[Rails.env].deep_symbolize_keys
+  Rails.configuration.elasticsearch = YAML.unsafe_load_file(ELASTICSEARCH_CONFIG_FILE)[Rails.env].deep_symbolize_keys
 
   Elasticsearch::Model.client = Elasticsearch::Client.new Rails.configuration.elasticsearch
 end
