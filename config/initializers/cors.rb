f7225134dea7df0a473432aafb76ba1416af6551
jdkim <jindong.kim@gmail.com>
2023-12-13 20:36:37 +0900

    REVISED the CORS setting

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -15,8 +15,10 @@
 #   end
 # end
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins '*'
-    resource '*', :headers => :any, :methods => [:get, :post, :options]
-  end
+	allow do
+		origins do |source, env|
+			source unless source == 'null'
+		end
+		resource '*', methods: :any, headers: :any, credentials: true
+	end
 end
