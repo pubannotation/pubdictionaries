8d7a6bba2fd72781be94449b313b57e574033bd2
Chisato Matoba <chisato.matoba@luxiar.com>
2024-02-21 18:05:50 +0900

    dictionaryごとのtagの一意性を追加

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,7 +3,7 @@ class Tag < ApplicationRecord
   has_many :entry_tags
   has_many :entries, through: :entry_tags
 
-  validates :value, length: {minimum: 3}
+  validates :value, length: {minimum: 3}, uniqueness: { scope: :dictionary_id }
   validates_format_of :value, # because of to_param overriding.
                       :with => /\A[a-zA-Z_][a-zA-Z0-9_\- ()]*\z/,
                       :message => "should begin with an alphabet or underscore, and only contain alphanumeric letters, underscore, hyphen, space, or round brackets!"
