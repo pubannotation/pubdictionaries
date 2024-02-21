d479037e7c08b35ad8ef6515ccd90795c90301e5
Chisato Matoba <chisato.matoba@luxiar.com>
2024-02-21 09:26:49 +0900

    バリデーション

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,4 +2,9 @@ class Tag < ApplicationRecord
   belongs_to :dictionary
   has_many :entry_tags
   has_many :entries, through: :entry_tags
+
+  validates :value, length: {minimum: 3}
+  validates_format_of :value, # because of to_param overriding.
+                      :with => /\A[a-zA-Z_][a-zA-Z0-9_\- ()]*\z/,
+                      :message => "should begin with an alphabet or underscore, and only contain alphanumeric letters, underscore, hyphen, space, or round brackets!"
 end
