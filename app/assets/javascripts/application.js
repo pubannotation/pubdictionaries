a5c007dfc0f73910764f0bb98aee9cbe632870dd
Chisato Matoba <chisato.matoba@luxiar.com>
2024-02-22 13:21:42 +0900

    select2設定

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,12 @@
 //= require wice_grid
 //= require clipboard
 //= require_tree .
+//= require select2
+
+document.addEventListener("DOMContentLoaded", function() {
+  $('.js-searchable').select2({
+    width: '100%',
+    allowClear: true,
+    containerCssClass: ':all:'
+  });
+});
