b5d72f3d9fc1f993a42ad6a009c211633e2a395c
Chisato Matoba <chisato.matoba@luxiar.com>
2024-03-05 11:23:05 +0900

    update_entries_numに置き換え

--- app/models/dictionary.rb
+++ app/models/dictionary.rb
@@ -157,11 +157,10 @@ class Dictionary < ApplicationRecord
 	def undo_entry(entry)
 		if entry.is_white?
 			entry.delete
-			decrement!(:entries_num)
 		elsif entry.is_black?
 			entry.be_gray!
-			increment!(:entries_num)
 		end
+		update_entries_num
 	end
 
   def update_entries_num
@@ -187,14 +186,14 @@ class Dictionary < ApplicationRecord
 	def turn_to_black(entry)
 		raise "Only a gray entry can be turned to black" unless entry.mode == Entry::MODE_GRAY
 		entry.be_black!
-		decrement!(:entries_num)
+		update_entries_num
 	end
 
 	# cancel a black entry to gray
 	def cancel_black(entry)
 		raise "Ony a black entry can be canceled to gray" unless entry.mode == Entry::MODE_BLACK
 		entry.be_gray!
-		increment!(:entries_num)
+		update_entries_num
 	end
 
 	def add_patterns(patterns)
@@ -216,7 +215,7 @@ class Dictionary < ApplicationRecord
 			r = Entry.bulk_import columns, entries, validate: false
 			raise "Import error" unless r.failed_instances.empty?
 
-			increment!(:entries_num, entries.length - black_count)
+			entries.first.dictionary.update_entries_num if entries.any?
 		end
 	end
 
@@ -241,20 +240,15 @@ class Dictionary < ApplicationRecord
 		when nil
 			transaction do
 				entries.destroy_all
-				update_attribute(:entries_num, 0)
 				clean_sim_string_db
 			end
 		when Entry::MODE_GRAY
-			_num_white = num_white
 			transaction do
 				entries.gray.destroy_all
-				update_attribute(:entries_num, _num_white)
 			end
 		when Entry::MODE_WHITE
-			_num_gray = num_gray
 			transaction do
 				entries.white.destroy_all
-				update_attribute(:entries_num, _num_gray)
 			end
 		when Entry::MODE_BLACK
 			transaction do
@@ -267,6 +261,7 @@ class Dictionary < ApplicationRecord
 		else
 			raise ArgumentError, "Unexpected mode: #{mode}"
 		end
+		update_entries_num
 	end
 
 	def new_pattern(expression, identifier)
