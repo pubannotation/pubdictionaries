df6f2b219e514e299c4ce60dd51dab8b5ca54a64
Chisato Matoba <chisato.matoba@luxiar.com>
2024-03-06 11:28:56 +0900

    ExpandSynonymJob作成

---
+++ app/jobs/expand_synonym_job.rb
@@ -0,0 +1,16 @@
+class ExpandSynonymJob < ApplicationJob
+  queue_as :general
+
+  def perform(dictionary)
+    dictionary.expand_synonym!
+  end
+
+  before_perform do |active_job|
+    set_job(active_job)
+    set_begun_at
+  end
+
+  after_perform do
+    set_ended_at
+  end
+end
