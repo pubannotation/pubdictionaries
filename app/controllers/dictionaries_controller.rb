131522012a58aab3739b11a05afd78bd28ece984
Chisato Matoba <chisato.matoba@luxiar.com>
2024-03-06 11:29:14 +0900

    expand_synonymアクション作成

--- app/controllers/dictionaries_controller.rb
+++ app/controllers/dictionaries_controller.rb
@@ -352,6 +352,25 @@ class DictionariesController < ApplicationController
 		end
 	end
 
+	def expand_synonym
+		begin
+			dictionary = Dictionary.editable(current_user).find_by_name(params[:id])
+			raise ArgumentError, "Cannot find the dictionary" if dictionary.nil?
+
+			active_job = ExpandSynonymJob.perform_later(dictionary)
+			active_job.create_job_record("Automatically expand synonyms for entries")
+
+			respond_to do |format|
+				format.html{ redirect_back fallback_location: root_path }
+			end
+		rescue => e
+			respond_to do |format|
+				format.html {redirect_to dictionary_path(dictionary), notice: e.message}
+				format.json {head :no_content}
+			end
+		end
+	end
+
 	def destroy
 		begin
 			dictionary = Dictionary.administrable(current_user).find_by_name(params[:id])
