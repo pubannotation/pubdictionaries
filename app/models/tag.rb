c78475e3aa50c105a8f7eec38940b831f13ec949
Chisato Matoba <chisato.matoba@luxiar.com>
2024-03-14 14:36:46 +0900

    tagバリデーションからspaceを削除

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -5,8 +5,8 @@ class Tag < ApplicationRecord
 
   validates :value, length: {minimum: 3}, uniqueness: { scope: :dictionary_id }
   validates_format_of :value, # because of to_param overriding.
-                      :with => /\A[a-zA-Z_][a-zA-Z0-9_\- ()]*\z/,
-                      :message => "should begin with an alphabet or underscore, and only contain alphanumeric letters, underscore, hyphen, space, or round brackets!"
+                      :with => /\A[a-zA-Z_][a-zA-Z0-9_\-()]*\z/,
+                      :message => "should begin with an alphabet or underscore, and only contain alphanumeric letters, underscore, hyphen, or round brackets, without space!"
 
   def used_in_entries?
     entry_tags.exists?
