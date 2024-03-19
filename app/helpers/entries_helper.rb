595e49eb8a1fc7f571badea13370fc96961d6a1c
Chisato Matoba <chisato.matoba@luxiar.com>
2024-03-19 18:05:20 +0900

    記述修正

--- app/helpers/entries_helper.rb
+++ app/helpers/entries_helper.rb
@@ -6,15 +6,15 @@ module EntriesHelper
   def pub_annotation_search_url(entry, dictionary)
     base_url = "https://pubannotation.org/term_search"
     query_params = {
-      "term_search_controller_query[block_type]" => 'sentence',
-      "term_search_controller_query[terms]" => entry.identifier,
-      "term_search_controller_query[predicates]" => '',
-      "term_search_controller_query[projects]" => '',
-      "term_search_controller_query[base_project]" => dictionary.associated_annotation_project,
-      "term_search_controller_query[page]" => 1,
-      "term_search_controller_query[per]" => 10,
+      block_type: 'sentence',
+      terms: entry.identifier,
+      predicates: '',
+      projects: '',
+      base_project: dictionary.associated_annotation_project,
+      page: 1,
+      per: 10,
     }
 
-    "#{base_url}?#{query_params.to_query}"
+    "#{base_url}?#{query_params.to_query(:term_search_controller_query)}"
   end
 end
