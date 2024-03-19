9b23e1c77f10caf770812caa1a1269a0993054f4
Chisato Matoba <chisato.matoba@luxiar.com>
2024-03-19 15:17:28 +0900

    URL生成部分を切り出し

--- app/helpers/entries_helper.rb
+++ app/helpers/entries_helper.rb
@@ -1,5 +1,9 @@
 module EntriesHelper
   def is_url?(id)
-  	id =~ /^https?:/
+    id =~ /^https?:/
+  end
+
+  def pub_annotation_search_url(entry, dictionary)
+    "https://pubannotation.org/term_search?term_search_controller_query%5Bblock_type%5D=sentence&term_search_controller_query%5Bterms%5D=#{entry.identifier}&term_search_controller_query%5Bpredicates%5D=&term_search_controller_query%5Bbase_project%5D=#{dictionary.associated_annotation_project}&term_search_controller_query%5Bprojects%5D=&term_search_controller_query%5Bpage%5D=1&term_search_controller_query%5Bper%5D=10"
   end
 end
