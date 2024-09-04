86e36eae5acea9c0e55ebd9d01aa39c137e2b856
yush-nh <yushin.nihei@luxiar.com>
2024-09-04 13:06:06 +0900

    Added notice for generate token

--- app/controllers/access_tokens_controller.rb
+++ app/controllers/access_tokens_controller.rb
@@ -2,11 +2,13 @@ class AccessTokensController < ApplicationController
   before_action :authenticate_user!
 
   def create
+    token_expires_in = Doorkeeper.configuration.access_token_expires_in
+
     token = Doorkeeper::AccessToken.create!(
       resource_owner_id: current_user.id,
-      expires_in: Doorkeeper.configuration.access_token_expires_in
+      expires_in: token_expires_in
     )
 
-    render json: { token: token.token }
+    render json: { token: token.token, expires_in: (token_expires_in / 3600) }
   end
 end
