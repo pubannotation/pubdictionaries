f0505ecfd245e1e33ae1b3f66fc5b88ddb72720c
yush-nh <yushin.nihei@luxiar.com>
2024-09-06 16:18:22 +0900

    Moved token logic from controller to model

--- app/models/access_token.rb
+++ app/models/access_token.rb
@@ -1,7 +1,23 @@
 class AccessToken < ApplicationRecord
   belongs_to :user
 
+  def self.find_token(headers)
+    bearer_token = bearer_token_in(headers)
+    find_by(token: bearer_token) if bearer_token
+  end
+
   def live?
     expired_at > Time.current
+  end
+
+  private
+
+  def self.bearer_token_in(headers)
+    case headers['Authorization']
+    in /^Bearer (.+)$/
+      Regexp.last_match(1)
+    else
+      nil
+    end
   end
 end
