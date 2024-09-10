a17fa8a6c69e32fd75a7898d65d51379ab6a5ef0
yush-nh <yushin.nihei@luxiar.com>
2024-09-10 15:46:19 +0900

    Fixed to set token by model callback

--- app/controllers/access_tokens_controller.rb
+++ app/controllers/access_tokens_controller.rb
@@ -2,7 +2,7 @@ class AccessTokensController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    current_user.create_access_token!(token: SecureRandom.hex(16))
+    current_user.create_access_token!
 
     redirect_back fallback_location: root_path, notice: "Access token was successfully created."
   end
--- app/models/access_token.rb
+++ app/models/access_token.rb
@@ -1,3 +1,10 @@
 class AccessToken < ApplicationRecord
   belongs_to :user
+  before_create :set_token
+
+  private
+
+  def set_token
+    self.token = SecureRandom.hex(16)
+  end
 end
