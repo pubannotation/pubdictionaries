e7bcf27c3b584e4d67048e26e3cc91dd019be7e6
yush-nh <yushin.nihei@luxiar.com>
2024-09-18 15:42:33 +0900

    Fixed normalize bug caused by operator expression

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -197,7 +197,7 @@ class Entry < ApplicationRecord
     _texts = texts.map { _1.tr('{}', '()') }
     body = { analyzer: normalizer, text: _texts }.to_json
     res = request_normalize(analyzer, body)
-    JSON.parse(res.body, symbolize_names: true)[:tokens].chunk_while { |a, b| b[:position] - a[:position] != INCREMENT_NUM_PER_LABEL }
+    JSON.parse(res.body, symbolize_names: true)[:tokens].chunk_while { |a, b| b[:position] - a[:position] < INCREMENT_NUM_PER_LABEL }
                                                         .map{|data| data.map{ _1[:token] }.join('') }
   end
 
