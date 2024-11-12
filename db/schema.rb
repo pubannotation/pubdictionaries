9d2b7b9037d404886354ebe4561a89235cfab13e
yush-nh <yushin.nihei@luxiar.com>
2024-11-12 13:35:26 +0900

    Run rails db:migrate and update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_10_064916) do
+ActiveRecord::Schema[8.0].define(version: 2024_09_10_064916) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
   enable_extension "pgcrypto"
-  enable_extension "plpgsql"
 
   create_table "access_tokens", force: :cascade do |t|
     t.bigint "user_id", null: false
